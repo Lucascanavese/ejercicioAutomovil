80d3e5758c4a59ae169e90b2587f62147e6bff9f
Lucascanavese <lucas.canavese@davinci.edu.ar>
2024-09-30 11:37:33 -0300

    ejercicio automovil

---
+++ automovil.py
@@ -0,0 +1,9 @@
+class Automovil:
+    def __init__(color,marca,aceleracion,velocidad,ruedas):
+        self.color= color
+        self.marca = marca
+        self.aceleracion = aceleracion
+        self.velocidad = velocidad
+        self.ruedas = ruedas
+    
+auto1 = Automovil()
